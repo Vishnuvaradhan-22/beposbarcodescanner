2c8d778fe7135bbac6d80a34cbf5c1bc2df77243
Vish <svaradhan92@gmail.com>
2017-04-27 01:22:47 +1000

    Integrated support for barcode scanning

--- BarcodeScanner/app/src/main/java/com/vectron/barcodescanner/ui/ScannerActivity.java
+++ BarcodeScanner/app/src/main/java/com/vectron/barcodescanner/ui/ScannerActivity.java
@@ -1,24 +1,69 @@
 package com.vectron.barcodescanner.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 
-
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.vectron.barcodescanner.R;
 
 public class ScannerActivity extends AppCompatActivity {
 
-
+    private Button scanButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_scanner);
         initializeUi();
     }
 
     private void initializeUi(){
+        Toolbar toolbar = (Toolbar)findViewById(R.id.action_menu_bar);
+        setSupportActionBar(toolbar);
 
+        scanButton = (Button)findViewById(R.id.btn_scan);
+        scanButton.setOnClickListener(scanButtonListener);
+
+    }
+
+    private OnClickListener scanButtonListener = new OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            scanBarcode();
+        }
+    };
+
+    private void scanBarcode(){
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.initiateScan();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            Log.d("result",scanResult.getContents());
+        }
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.scanner_activity,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.configure_page) {
+            Intent intent = new Intent();
+            intent.setClass(getApplicationContext(), ConfigurationActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
