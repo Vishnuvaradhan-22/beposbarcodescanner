3ad93b3ae5151524fcbbd3087ad3308af7d188c8
Vish <svaradhan92@gmail.com>
2017-11-05 18:41:45 +1100

    Added feature to print in zebra printer

---
+++ BarcodeScanner/app/src/main/java/com/vectron/barcodescanner/ui/CustomRequest.java
@@ -0,0 +1,66 @@
+package com.vectron.barcodescanner.ui;
+
+/**
+ * Created by Vish on 27/09/2017.
+ */
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+public class CustomRequest extends Request<JSONObject> {
+
+    private Listener<JSONObject> listener;
+    private Map<String, String> params;
+
+    public CustomRequest(String url, Map<String, String> params,
+                         Listener<JSONObject> reponseListener, ErrorListener errorListener) {
+        super(Method.POST, url, errorListener);
+        this.listener = reponseListener;
+        this.params = params;
+    }
+
+    public CustomRequest(int method, String url, Map<String, String> params,
+                         Listener<JSONObject> reponseListener, ErrorListener errorListener) {
+        super(method, url, errorListener);
+        this.listener = reponseListener;
+        this.params = params;
+    }
+
+    protected Map<String, String> getParams()
+            throws com.android.volley.AuthFailureError {
+        return params;
+    };
+
+    @Override
+    public String getBodyContentType() {
+        return "application/x-www-form-urlencoded; charset=UTF-8";
+    }
+
+    @Override
+    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers));
+            return Response.success(new JSONObject(jsonString),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JSONException je) {
+            return Response.error(new ParseError(je));
+        }
+    }
+
+    @Override
+    protected void deliverResponse(JSONObject response) {
+        // TODO Auto-generated method stub
+        listener.onResponse(response);
+    }
+}
---
+++ BarcodeScanner/app/src/main/java/com/vectron/barcodescanner/ui/ViewHistoryActivity.java
@@ -0,0 +1,15 @@
+package com.vectron.barcodescanner.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vectron.barcodescanner.R;
+
+public class ViewHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_history);
+    }
+}
